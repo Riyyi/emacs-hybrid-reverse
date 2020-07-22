7af5aea9a8d1330d7f5f1e69cec774d984fcf621
Riyyi <riyyi3@gmail.com>
2020-07-22 16:58:25 +0200

    Update package requirements to the correct version

--- hybrid-reverse-theme.el
+++ hybrid-reverse-theme.el
@@ -3,7 +3,7 @@
 ;; Author: Riyyi
 ;; URL: https://github.com/riyyi/emacs-hybrid-reverse
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "24.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
